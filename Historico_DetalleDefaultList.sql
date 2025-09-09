330426760773896aa1844901f1037a82355e18b0
klkelpacoo <franciscofernandezrodriguez00@gmail.com>
2025-09-09 10:07:18 +0200

    Create Historico_DetalleDefaultList.sql

---
+++ Historico_DetalleDefaultList.sql
@@ -0,0 +1,2 @@
+ SELECT [V_DetalleMovimientos].[ID_Unico], [V_DetalleMovimientos].[ID_Unico] as [ID_Unico_1], [V_DetalleMovimientos].[Usuario] as [Usuario], [V_DetalleMovimientos].[FechaInsertUpdate] as [FechaInsertUpdate], [V_DetalleMovimientos].[NombreAlmacen] as [NombreAlmacen], [V_DetalleMovimientos].[CodBarras1] as [CodBarras1], [V_DetalleMovimientos].[Destino] as [Destino] FROM [V_DetalleMovimientos] 
+
