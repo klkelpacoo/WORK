3d2d276980923df7a0f6c811138824ab807e2eb2
klkelpacoo <franciscofernandezrodriguez00@gmail.com>
2025-09-09 10:08:50 +0200

    Update Historico_DetalleDefaultList.sql

--- Historico_DetalleDefaultList.sql
+++ Historico_DetalleDefaultList.sql
@@ -1,2 +1,3 @@
  SELECT [V_DetalleMovimientos].[ID_Unico], [V_DetalleMovimientos].[ID_Unico] as [ID_Unico_1], [V_DetalleMovimientos].[Usuario] as [Usuario], [V_DetalleMovimientos].[FechaInsertUpdate] as [FechaInsertUpdate], [V_DetalleMovimientos].[NombreAlmacen] as [NombreAlmacen], [V_DetalleMovimientos].[CodBarras1] as [CodBarras1], [V_DetalleMovimientos].[Destino] as [Destino] FROM [V_DetalleMovimientos] 
-
+SELECT * FROM dbo.V_DetalleMovimientos WHERE Usuario = '{{p_usuario}}'
+p_usuario
